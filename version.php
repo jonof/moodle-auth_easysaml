0591c10adfe9b91b22d86c195806d197e3894972
Jonathon Fowler <jf@jonof.id.au>
2015-03-19 10:33:39 +1000

    lower required Moodle version to 2.6

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2015031200;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014110400;        // Requires this Moodle version
+$plugin->requires  = 2013111800;        // Requires this Moodle version
 $plugin->component = 'auth_simplesaml'; // Full name of the plugin (used for diagnostics)
